6feee7baccc13a33f795f782c142baad786401a5
Monikku12 <monique.paymentboulanger@gmail.com>
2022-06-18 13:48:27 +0900

    Class and file structure : Done

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Rocket-Elevators-Ruby-Controller
+
+This is the first part of the Ruby version. I hope finish it by the end of the weekend. 
+
+# Description
+
+This program controls a Column of elevators.
+
+It sends an elevator fetch a user when a button on a floor is press and it takes
+this user to its desired floor when the corresponding button inside the elevator is pressed.
+
+The program first creates the columns, elevators, buttons and other variables it needs to operate depending on the data you will input. From then, the program operates and controls the elevators in accordance of you scenario.
+
+The elevator selection is based on a calculation determinating the best elevator possible. This calculation considers the status of the elevator, its position and its direction if it's moving.
+
+# Dependencies
+
+To be able to run the program, you need to have the Ruby language installed. 
+
+# Usage
+
+To use the program, you need to fill the TEMPLATE section at the bottom of the file. Fill the TEMPLATE by entering your own data by replacing the text with the <>. 
+
+## Example
+
+<!-- TEMPLATE - empty - To be adjust -->
+<!-- var templateColumn = new Column(1, <numberOfFloors>, <numberOfElevators>);
+templateColumn.elevatorList[0].currentFloor = <yourFirstElevatorCurrentFloor>;
+templateColumn.elevatorList[1].currentFloor = <yourSecondElevatorCurrentFloor>;
+var elevator = templateColumn.requestElevator(<yourCurrentFloor>, "<yourRequestedDirection>");
+elevator.requestFloor(<yourRequestedFloorNumber>); -->
+
+<!-- TEMPLATE - filled- To be adjust -->
+<!-- var templateColumn = new Column(1, 10, 2);
+templateColumn.elevatorList[0].currentFloor = 10;
+templateColumn.elevatorList[1].currentFloor = 3;
+var elevator = templateColumn.requestElevator(1, "up");
+elevator.requestFloor(6); -->
+
+Which means you have a building with one column of 2 elevators. The first elevator is on the 10th floor and the second elevator is on the third floor. Someone enters the building and press the button to call the elevator. In this case, the best elevator to reach the users will be the second elevator. The program will then operate the second elevator to pick up the users and bring him to the 6F like requested. 
---
+++ residential_controller.rb
@@ -0,0 +1,158 @@
+
+
+
+
+class Column 
+    attr_accessor :elevatorList, :callButtonList, :status, :ID
+    def initialize(_id, _amountOfFloors, _amountOfElevators) 
+        @ID = 1
+        @status = "online"
+        @elevatorList = []
+        @callButtonList = []
+        createElevators(_amountOfFloors, _amountOfElevators)
+        createCallButtons(_amountOfFloors)
+    end
+
+
+        # ---------------------------------Methods--------------------------------------------
+        createCallButtons( _amountOfFloors)
+
+
+
+
+
+
+
+        createElevators(_amountOfFloors, _amountOfElevators)
+
+
+
+
+
+
+
+        # Simulate when a user press a button outside the elevator
+        requestElevator(floor, direction)
+
+
+
+
+
+
+
+        # We use a score system depending on the current elevators state. Since the bestScore and the referenceGap are
+        # higher values than what could be possibly calculated, the first elevator will always become the default bestElevator,
+        # before being compared with to other elevators. If two elevators get the same score, the nearest one is prioritized.
+        findElevator(requestedFloor, requestedDirection)
+
+
+
+
+
+
+
+
+
+
+        checkIfElevatorIsBetter(scoreToCheck, newElevator, bestElevatorInformations, floor):
+
+
+end
+
+
+
+class Elevator:
+    attr_accessor :elevatorList, :callButtonList, :status, :ID
+    def initialize(_id, _amountOfFloors)
+        @ID = _id
+        @status = "idle"
+        @currentFloor = 1
+        @amountOfFloors = _amountOfFloors
+        @direction = None
+        @door = Door(_id) 
+        @floorRequestButtonList = []
+        @floorRequestList = []
+        createFloorRequestButtons(_amountOfFloors)
+    end
+
+        createFloorRequestButtons(_amountOfFloors)
+
+
+
+
+
+
+        # Simulate when a user press a button inside the elevator
+        requestFloor(floor)
+
+
+
+
+        move()
+
+
+
+
+
+
+
+
+
+        sortFloorList()
+
+
+
+
+
+
+
+
+
+        operateDoors()
+
+
+
+
+
+
+
+end
+
+class CallButton:
+    def initialize( _id, _floor, _direction):
+        @ID = _id
+        @status = "OFF"
+        @floor = _floor
+        @direction = _direction
+    end
+end
+
+class FloorRequestButton:
+    def initialize(_id, _floor):
+        @ID = _id
+        @status = "OFF"
+        @floor = _floor
+    end
+end
+
+class Door:
+    def initialize(_id):
+        @ID = _id
+        @status = "closed"
+    end
+end
+
+# ******************
+
+# To run your own simulation, uncomment the TEMPLATE section below and enter your own value in the different fields. 
+
+# TEMPLATE - To be adjust
+# templateColumn = Column(1, <numberOfFloors>, <numberOfElevators>)
+# templateColumn.elevatorList[0].currentFloor = <yourFirstElevatorCurrentFloor>
+# templateColumn.elevatorList[1].currentFloor = <yourSecondElevatorCurrentFloor>
+# elevator = templateColumn.requestElevator(<yourCurrentFloor>, "<yourRequestedDirection>")
+# elevator.requestFloor(<yourRequestedFloorNumber>)
+
+#  ******************
+
+
